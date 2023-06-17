d874cfc2e2086c86746065be714dcc8dbd684850
unaivan <unaivan@gmail.com>
2023-06-17 09:18:57 +0700

    Initial Commit

---
+++ Rumahku/RumahkuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RumahkuApp.swift
+//  Rumahku
+//
+//  Created by una ivan on 17/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RumahkuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
